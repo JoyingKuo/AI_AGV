18217e3aa8cf312731e2719120987c7d98d98d1e
JoyingKuo <joying62757@gmail.com>
2019-12-18 01:24:51 +0800

    code format for predict single view

--- test/predict_single_image.py
+++ test/predict_single_image.py
@@ -6,51 +6,60 @@ import cv2
 import os
 import sys
 
-# configure path
-model_name = '../../data/model/model_dir_ver4/12.7279729479321557_Nov-13-2019_10-08-49.h5'
-image_path = '../../data/108-10-16/test -4/2019-10-16-15-16-49-1184-p078-p080.png'
-def load_in_img(img_location):
+# 環境變數設置
+os.environ['HDF5_USE_FILE_LOCKING']='FALSE' #為了避免 h5py default 的 file lock, 存model的時候才不會被拒絕 
+
+# 路徑初始化
+loaded_model_path = '../../data/model/model_dir_ver4/12.7279729479321557_Nov-13-2019_10-08-49.h5' # 要被載入的 model 路徑
+image_path = '../../data/108-10-16/test -4/2019-10-16-15-16-49-1184-p078-p080.png' # 要被評估的照片路徑
+
+#參數初始化
+
+# test image 要被resize 到跟training image 一樣的大小才可以被 predict
+image_reshape_width = 400
+image_reshape_height = 400  
+
+
+def load_in_img(image_path):
     
-    #folder name has save in img_location
-    imageLocation = img_location
-    image = cv2.imread(imageLocation) # Gray
+    # 根據 image_path 讀取照片, 將其resize成(400,400)
+    # 轉換色相至灰階並 reshape 其至 1 channel
+    img = cv2.imread(image_path) 
+    img = cv2.resize(img, (image_reshape_width, image_reshape_height), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = img.reshape(image_reshape_width, image_reshape_height, 1)
 
-    if (image is None):
-        print(imageLocation)
-#     print(imageLocation)
+    #  轉換image 至可以被餵進 model 的資料型態
+    img = image.img_to_array(img) 
+    img = np.expand_dims(img, axis=0) 
+    img = np.vstack([img])
 
-#     image = image[45:-9,::]
-    image = cv2.resize(image, (400, 400), interpolation=cv2.INTER_CUBIC)
-    #image = color_frame_process(image)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    image = image.reshape(400, 400, 1)
-    return image
-
-# dimensions of our images
-img_width, img_height = 320, 240
-os.environ['HDF5_USE_FILE_LOCKING']='FALSE'
-
-# +
-# load the model we saved
-
-print(model_name)
-model = load_model(model_name)
-model.compile(optimizer="adam", loss="mse")
-# -
-
-# predicting images
-# img = load_in_img('./dir/test - 9/black.png')#black
-# img = load_in_img('./dir/test - 改-第二個轉彎後繼續前進/2019-06-13-15-15-44-5910-p059-p058.png')#black
-img = load_in_img(image_path)#second turn
-
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
+    return img
 
 
-images = np.vstack([x])
-classes = model.predict(images, batch_size=10)
-print ('op classes: ')
-print (classes)
+# 利用訓練好的model, 輸入照片並檢視其網路輸出結果
+def predict(model,image_path)
+    
+    # 載入照片並做資料前處理
+    img = load_in_img(image_path)
 
+    # 預測結果
+    result = model.predict(img, batch_size=10)
 
+    return result
+
+def main():
+    # 載入先前訓練好的 model
+    model = load_model(loaded_model)
+    model.compile(optimizer="adam", loss="mse")
+
+    #評估input image的結果
+    result = predict(model, image_path)
+    print ('result: ')
+    print (result)
+
+    
+
+if __name__ == '__main__':
+    main()
 
